ecf004a95d3e445bb4e556593354e955f13a1515
angela cuadros <angelascc16@gmail.com>
2022-01-21 13:34:31 -0500

    style scss and bookmark(destroy) added

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root to: 'lists#index'
   resources :lists, only: [:index, :new, :show, :create, :destroy] do
     resources :bookmarks, only: [:new, :create]
-    #resources :movies, only: [:new, :create]
   end
-
+  resources :bookmarks, only: :destroy
 end
