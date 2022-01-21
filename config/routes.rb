ba1f8547970ef9ec350e613f3904584c7d6e890f
angela cuadros <angelascc16@gmail.com>
2022-01-20 22:40:38 -0500

    first versionn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
+  resources :lists, only: [:index, :new, :show, :create, :destroy] do
+    resources :bookmarks, only: [:new, :create]
+    #resources :movies, only: [:new, :create]
+  end
+
 end
